6c045080b149112d3acbfeb61ca03d626d7be861
0qtist <77591116+oqtist@users.noreply.github.com>
2024-07-23 03:47:50 -0300

    Refeito o código.

--- personagens.js
+++ personagens.js
@@ -1,271 +1,73 @@
-// listar os itens que foram adicionados nos inputs do form
-let informacaoPersonagem = []
-const divMenu = document.querySelector("#adicionar-personagem")
+const divMenu = document.querySelector("#div-popup")
+const divPersonagens = document.querySelector('#div-personagens')
+const divPopupChars = document.querySelector('#div-popup-chars')
 
-function fecharPopupJanelinha() {
-    divPopupPersonagem.classList.toggle('oculta')
-
+function fecharPopup() {
+    divMenu.classList.toggle('oculta')
+    divPopupChars.classList.add('oculta')
+}
+function fecharPopupChar () {
+    divPopupChars.classList.toggle('oculta')
 }
 
+let arrayPersonagens = []
 
-// função de enviar e verificar se há informações no campo 
-function enviarProtagonista(evento) {
+if (localStorage.getItem('arraySalvoChars')) {
+    arrayPersonagens = JSON.parse(localStorage.getItem('arraySalvoChars'))
+    addDivChars()
+}
+
+function enviarPersonagem(evento) {
     evento.preventDefault()
 
     const form = document.querySelector('form')
     const inputs = new FormData(form)
 
-    const fotoCapa = inputs.get('protagonista_foto')
-    const nomePerso = inputs.get('protagonista_nome')
-    const nomeReal = inputs.get('nome_real')
-    const filme = inputs.get('nome_filme')
-    const habil = inputs.get('habilidades')
-    const descri = inputs.get('descricao')
+    let charEnviado = []
 
-    if (fotoCapa && nomePerso && nomeReal && filme && habil && descri) {
-        informacaoPersonagem.push(fotoCapa, "Nome: " + nomePerso, "Ator/atriz:" + nomeReal, "Filme: " + filme, "Habilidades: " + habil, "Descrição: " + descri)
-        const conversaoArray = JSON.stringify(informacaoPersonagem)
-        localStorage.setItem("infosArray", conversaoArray)
+    charEnviado.push(inputs.get('foto-char'),inputs.get('nome'),inputs.get('actor'),inputs.get('nome-filme'),inputs.get('habilidades'),inputs.get('descricao'))
+
+    arrayPersonagens.push(charEnviado)
+    addDivChars()
+
+    const arrayConvertido = JSON.stringify(arrayPersonagens)
+    localStorage.setItem("arraySalvoChars", arrayConvertido)
+}
+
+
+function addDivChars() {
+    divPersonagens.innerHTML = '<div id="div-adicionar-char"><p>Adicionar Personagem</p><button onclick="fecharPopup()">+</button></div>'
+    for(i=0;i<arrayPersonagens.length;i++) {
+        let a = divPersonagens.innerHTML += '<div onclick="menuChar(event)" class="class-char-covers" id="'+(i)+'"></div>'
+        const charAtual = document.getElementById(i)
+        const arrayAtual = arrayPersonagens[i]
+        const capaAtual = arrayAtual[0]
+        charAtual.style.backgroundImage = `url(${capaAtual})`
+    }
+    const arrayConvertido = JSON.stringify(arrayPersonagens)
+    localStorage.setItem("arraySalvoChars", arrayConvertido)
+}
+
+let checkSameChar
+
+function menuChar(evento) {
+    divPopupChars.innerHTML = (`<h2>Informações do/a Personagem:</h2><button class="fechar-popup" onclick="fecharPopupChar()">x</button>`)
+    const charSelecionado = arrayPersonagens[evento.target.id]
+
+    if(checkSameChar == evento.target.id) {
+        divPopupChars.classList.toggle('oculta')
+        divMenu.classList.add('oculta')
+    } else {
+        divPopupChars.classList.remove('oculta')
         divMenu.classList.add('oculta')
     }
+    divPopupChars.innerHTML += (`Nome: ${charSelecionado[1]}<br>Intérprete: ${charSelecionado[2]}<br>Nome do Filme: ${charSelecionado[3]}<br>Habilidades: ${charSelecionado[4]}<br>Descrição: ${charSelecionado[5]}<br><button id="botao-apagar-char" onclick="removerChar(event)">Remover Personagem</button>`)
 
-    mostrarImagem()
+    checkSameMovie = evento.target.id
 }
 
-if (localStorage.getItem('infosArray')) {
-    informacaoPersonagem = JSON.parse(localStorage.getItem('infosArray'))
-}
-
-const perso1 = document.querySelector("#perso1")
-const perso2 = document.querySelector("#perso2")
-const perso3 = document.querySelector("#perso3")
-const perso4 = document.querySelector("#perso4")
-const perso5 = document.querySelector("#perso5")
-const perso6 = document.querySelector("#perso6")
-const perso7 = document.querySelector("#perso7")
-const perso8 = document.querySelector("#perso8")
-const perso9 = document.querySelector("#perso9")
-const perso10 = document.querySelector("#perso10")
-const perso11 = document.querySelector("#perso11")
-
-function mostrarImagem() {
-    switch (true) {
-        case informacaoPersonagem.length >= 66:
-            divMenu.innerHTML = ('A quantidade máxima de personagens foi alcaçada. Remova um personagem para continuar.')
-            perso11.style.backgroundImage = `url("${informacaoPersonagem[60]}")`
-
-        case informacaoPersonagem.length >= 6:
-            perso1.style.backgroundImage = `url("${informacaoPersonagem[0]}")`
-
-        case informacaoPersonagem.length >= 12:
-            perso2.style.backgroundImage = `url("${informacaoPersonagem[6]}")`
-
-        case informacaoPersonagem.length >= 18:
-            perso3.style.backgroundImage = `url("${informacaoPersonagem[12]}")`
-
-        case informacaoPersonagem.length >= 24:
-            perso4.style.backgroundImage = `url("${informacaoPersonagem[18]}")`
-
-        case informacaoPersonagem.length >= 30:
-            perso5.style.backgroundImage = `url("${informacaoPersonagem[24]}")`
-
-        case informacaoPersonagem.length >= 36:
-            perso6.style.backgroundImage = `url("${informacaoPersonagem[30]}")`
-
-        case informacaoPersonagem.length >= 42:
-            perso7.style.backgroundImage = `url("${informacaoPersonagem[36]}")`
-
-        case informacaoPersonagem.length >= 48:
-            perso8.style.backgroundImage = `url("${informacaoPersonagem[42]}")`
-
-        case informacaoPersonagem.length >= 54:
-            perso9.style.backgroundImage = `url("${informacaoPersonagem[48]}")`
-
-        case informacaoPersonagem.length >= 60:
-            perso10.style.backgroundImage = `url("${informacaoPersonagem[54]}")`
-            break
-        default:
-            perso1.style.backgroundImage = ''
-            break
-    }
-}
-
-
-
-// abrir e fechar o popup
-const popUp = document.querySelector('#pop-up')
-function fecharPopup() {
-    popUp.classList.toggle('oculta')
-}
-
-let contador = 0
-const divPopupPersonagem = document.querySelector('#div-popup-perso')
-
-function popPerso(evento) {
-    divPopupPersonagem.innerHTML = (`<button onclick="fecharPopupJanelinha()" class="fechar-popup">x</button>`)
-    switch (evento.target.id) {
-        case "perso1":
-            if (informacaoPersonagem[1]) {
-                for (i = 1; i < 6; i++) {
-                    divPopupPersonagem.innerHTML += (`${informacaoPersonagem[i]}<br>`)
-                }
-                divPopupPersonagem.classList.toggle('oculta')
-            }
-            contador = 1
-            divPopupPersonagem.innerHTML += (`<button onclick="apagarPerso()" id="botao-apagar-perso">Apagar Personagem</button>`)
-            break
-
-        case "perso2":
-            if (informacaoPersonagem[7]) {
-                for (i = 7; i < 12; i++) {
-                    divPopupPersonagem.innerHTML += (`${informacaoPersonagem[i]}<br>`)
-                }
-                divPopupPersonagem.classList.toggle('oculta')
-            }
-            contador = 2
-            divPopupPersonagem.innerHTML += (`<button onclick="apagarPerso()" id="botao-apagar-perso">Apagar Personagem</button>`)
-            break
-
-        case "perso3":
-            if (informacaoPersonagem[13]) {
-                for (i = 13; i < 18; i++) {
-                    divPopupPersonagem.innerHTML += (`${informacaoPersonagem[i]}<br>`)
-                }
-                divPopupPersonagem.classList.toggle('oculta')
-            }
-            contador = 3
-            divPopupPersonagem.innerHTML += (`<button onclick="apagarPerso()" id="botao-apagar-perso">Apagar Personagem</button>`)
-            break
-
-        case "perso4":
-            if (informacaoPersonagem[19]) {
-                for (i = 19; i < 24; i++) {
-                    divPopupPersonagem.innerHTML += (`${informacaoPersonagem[i]}<br>`)
-                }
-                divPopupPersonagem.classList.toggle('oculta')
-            }
-            contador = 4
-            divPopupPersonagem.innerHTML += (`<button onclick="apagarPerso()" id="botao-apagar-perso">Apagar Personagem</button>`)
-            break
-
-        case "perso5":
-            if (informacaoPersonagem[25]) {
-                for (i = 25; i < 30; i++) {
-                    divPopupPersonagem.innerHTML += (`${informacaoPersonagem[i]}<br>`)
-                }
-                divPopupPersonagem.classList.toggle('oculta')
-            }
-            contador = 5
-            divPopupPersonagem.innerHTML += (`<button onclick="apagarPerso()" id="botao-apagar-perso">Apagar Personagem</button>`)
-            break
-
-        case "perso6":
-            if (informacaoPersonagem[31]) {
-                for (i = 31; i < 36; i++) {
-                    divPopupPersonagem.innerHTML += (`${informacaoPersonagem[i]}<br>`)
-                }
-                divPopupPersonagem.classList.toggle('oculta')
-            }
-            contador = 6
-            divPopupPersonagem.innerHTML += (`<button onclick="apagarPerso()" id="botao-apagar-perso">Apagar Personagem</button>`)
-            break
-
-        case "perso7":
-            if (informacaoPersonagem[37]) {
-                for (i = 37; i < 42; i++) {
-                    divPopupPersonagem.innerHTML += (`${informacaoPersonagem[i]}<br>`)
-                }
-                divPopupPersonagem.classList.toggle('oculta')
-            }
-            contador = 7
-            divPopupPersonagem.innerHTML += (`<button onclick="apagarPerso()" id="botao-apagar-perso">Apagar Personagem</button>`)
-            break
-
-        case "perso8":
-            if (informacaoPersonagem[43]) {
-                for (i = 43; i < 48; i++) {
-                    divPopupPersonagem.innerHTML += (`${informacaoPersonagem[i]}<br>`)
-                }
-                divPopupPersonagem.classList.toggle('oculta')
-            }
-            contador = 8
-            divPopupPersonagem.innerHTML += (`<button onclick="apagarPerso()" id="botao-apagar-perso">Apagar Personagem</button>`)
-            break
-
-        case "perso9":
-            if (informacaoPersonagem[49]) {
-                for (i = 49; i < 54; i++) {
-                    divPopupPersonagem.innerHTML += (`${informacaoPersonagem[i]}<br>`)
-                }
-                divPopupPersonagem.classList.toggle('oculta')
-            }
-            contador = 9
-            divPopupPersonagem.innerHTML += (`<button onclick="apagarPerso()" id="botao-apagar-perso">Apagar Personagem</button>`)
-            break
-
-        case "perso10":
-            if (informacaoPersonagem[55]) {
-                for (i = 55; i < 60; i++) {
-                    divPopupPersonagem.innerHTML += (`${informacaoPersonagem[i]}<br>`)
-                }
-                divPopupPersonagem.classList.toggle('oculta')
-            }
-            contador = 10
-            divPopupPersonagem.innerHTML += (`<button onclick="apagarPerso()" id="botao-apagar-perso">Apagar Personagem</button>`)
-            break
-
-        case "perso11":
-            if (informacaoPersonagem[61]) {
-                for (i = 61; i < 66; i++) {
-                    divPopupPersonagem.innerHTML += (`${informacaoPersonagem[i]}<br>`)
-                }
-                divPopupPersonagem.classList.toggle('oculta')
-            }
-            contador = 11
-            divPopupPersonagem.innerHTML += (`<button onclick="apagarPerso()" id="botao-apagar-perso">Apagar Personagem</button>`)
-            break
-
-    }
-}
-
-function apagarPerso() {
-    switch (contador) {
-        case 1:
-            informacaoPersonagem.splice(0, 6)
-            break
-        case 2:
-            informacaoPersonagem.splice(6, 6)
-            break
-        case 3:
-            informacaoPersonagem.splice(12, 6)
-            break
-        case 4:
-            informacaoPersonagem.splice(18, 6)
-            break
-        case 5:
-            informacaoPersonagem.splice(24, 6)
-            break
-        case 6:
-            informacaoPersonagem.splice(30, 6)
-            break
-        case 7:
-            informacaoPersonagem.splice(36, 6)
-            break
-        case 8:
-            informacaoPersonagem.splice(42, 6)
-            break
-        case 9:
-            informacaoPersonagem.splice(48, 6)
-            break
-        case 10:
-            informacaoPersonagem.splice(54, 6)
-            break
-        case 11:
-            informacaoPersonagem.splice(60, 6)
-            break
-    }
-    divPopupPersonagem.classList.add('oculta')
-    const conversaoArray = JSON.stringify(informacaoPersonagem)
-    localStorage.setItem("infosArray", conversaoArray)
-    mostrarImagem()
+function removerChar(evento) {
+    arrayPersonagens.splice(evento.target.id,1)
+    fecharPopupChar()
+    addDivChars()
 }
--- script.js
+++ script.js
@@ -1,28 +1,21 @@
+const divMenu = document.querySelector("#div-popup")
+const divFilmes = document.querySelector('#div-filmes')
+const divPopupFilme = document.querySelector('#div-popup-filme')
 
-const popup = document.querySelector('#div-popup')
 function fecharPopup() {
-    popup.classList.toggle('oculta')
+    divMenu.classList.toggle('oculta')
+    divPopupFilme.classList.add('oculta')
 }
-function fecharPopupFilme() {
+function fecharPopupFilme () {
     divPopupFilme.classList.toggle('oculta')
 }
 
-const divPopupFilme = document.querySelector('#div-popup-filme')
-const botaoFecharPopupFilme = document.querySelector('#botao-fechar-filme')
+let arrayFilmes = []
 
-let informacoesFilmes = []
-const filme1 = document.querySelector('#filme-1')
-const filme2 = document.querySelector('#filme-2')
-const filme3 = document.querySelector('#filme-3')
-const filme4 = document.querySelector('#filme-4')
-const filme5 = document.querySelector('#filme-5')
-const filme6 = document.querySelector('#filme-6')
-const filme7 = document.querySelector('#filme-7')
-const filme8 = document.querySelector('#filme-8')
-const filme9 = document.querySelector('#filme-9')
-const filme10 = document.querySelector('#filme-10')
-const filme11 = document.querySelector('#filme-11')
-const divMenu = document.querySelector('#div-adicionar-filme')
+if (localStorage.getItem('arraySalvo')) {
+    arrayFilmes = JSON.parse(localStorage.getItem('arraySalvo'))
+    addDivFilmes()
+}
 
 function enviarFilme(evento) {
     evento.preventDefault()
@@ -30,222 +23,51 @@ function enviarFilme(evento) {
     const form = document.querySelector('form')
     const inputs = new FormData(form)
 
-    const urlCapa = inputs.get('foto-da-capa')
-    const titulo = inputs.get('titulo')
-    const diretor = inputs.get('diretor')
-    const dataLancamento = inputs.get('data-lancamento')
-    const genero = inputs.get('genero')
-    const sinopse = inputs.get('sinopse')
+    let filmeEnviado = []
 
-    if (urlCapa && titulo && diretor && dataLancamento && genero && sinopse) {
-        informacoesFilmes.push(urlCapa, 'Título: ' + titulo, 'Diretor: ' + diretor, 'Ano de Lançamento: ' + dataLancamento, 'Gênero: ' + genero, 'Sinopse: ' + sinopse)
+    filmeEnviado.push(inputs.get('foto-da-capa'),inputs.get('titulo'),inputs.get('diretor'),inputs.get('data-lancamento'),inputs.get('genero'),inputs.get('sinopse'))
 
-        const arrayConvertido = JSON.stringify(informacoesFilmes)
-        localStorage.setItem('arraySalvo', arrayConvertido)
-        popup.classList.add('oculta')
+    arrayFilmes.push(filmeEnviado)
+    addDivFilmes()
+
+    const arrayConvertido = JSON.stringify(arrayFilmes)
+    localStorage.setItem("arraySalvo", arrayConvertido)
+}
+
+
+function addDivFilmes() {
+    divFilmes.innerHTML = '<div id="div-adicionar-filme"><p>Adicionar Filme</p><button onclick="fecharPopup()">+</button></div>'
+    for(i=0;i<arrayFilmes.length;i++) {
+        let a = divFilmes.innerHTML += '<div onclick="menuFilme(event)" class="class-filme-covers" id="'+(i)+'"></div>'
+        const filmeAtual = document.getElementById(i)
+        const arrayAtual = arrayFilmes[i]
+        const capaAtual = arrayAtual[0]
+        filmeAtual.style.backgroundImage = `url(${capaAtual})`
     }
-    switchCarregar()
-
+    const arrayConvertido = JSON.stringify(arrayFilmes)
+    localStorage.setItem("arraySalvo", arrayConvertido)
 }
 
-if (localStorage.getItem('arraySalvo')) {
-    informacoesFilmes = JSON.parse(localStorage.getItem('arraySalvo'))
-}
+let checkSameMovie
 
-function switchCarregar() {
-    switch (true) {
-        case informacoesFilmes.length >= 66:
-            divMenu.innerHTML = (`<p>O total de filmes foi alcançado. Tente remover um filme.</p>`)
-            filme11.style.backgroundImage = `url("${informacoesFilmes[60]}")`;
-        case informacoesFilmes.length >= 6:
-            filme1.style.backgroundImage = `url("${informacoesFilmes[0]}")`;
-        case informacoesFilmes.length >= 12:
-            filme2.style.backgroundImage = `url("${informacoesFilmes[6]}")`;
-        case informacoesFilmes.length >= 18:
-            filme3.style.backgroundImage = `url("${informacoesFilmes[12]}")`;
-        case informacoesFilmes.length >= 24:
-            filme4.style.backgroundImage = `url("${informacoesFilmes[18]}")`;
-        case informacoesFilmes.length >= 30:
-            filme5.style.backgroundImage = `url("${informacoesFilmes[24]}")`;
-        case informacoesFilmes.length >= 36:
-            filme6.style.backgroundImage = `url("${informacoesFilmes[30]}")`;
-        case informacoesFilmes.length >= 42:
-            filme7.style.backgroundImage = `url("${informacoesFilmes[36]}")`;
-        case informacoesFilmes.length >= 48:
-            filme8.style.backgroundImage = `url("${informacoesFilmes[42]}")`;
-        case informacoesFilmes.length >= 54:
-            filme9.style.backgroundImage = `url("${informacoesFilmes[48]}")`;
-        case informacoesFilmes.length >= 60:
-            filme10.style.backgroundImage = `url("${informacoesFilmes[54]}")`;
-            break
-        default:
-            filme1.style.backgroundImage = ''
-            break
+function menuFilme(evento) {
+    divPopupFilme.innerHTML = (`<h2>Informações do Filme:</h2><button class="fechar-popup" onclick="fecharPopupFilme()">x</button>`)
+    const filmeSelecionado = arrayFilmes[evento.target.id]
+
+    if(checkSameMovie == evento.target.id) {
+        divPopupFilme.classList.toggle('oculta')
+        divMenu.classList.add('oculta')
+    } else {
+        divPopupFilme.classList.remove('oculta')
+        divMenu.classList.add('oculta')
     }
+    divPopupFilme.innerHTML += (`Título do Filme: ${filmeSelecionado[1]}<br>Diretor: ${filmeSelecionado[2]}<br>Data de Lançamento: ${filmeSelecionado[3]}<br>Gênero: ${filmeSelecionado[4]}<br>Sinopse: ${filmeSelecionado[5]}<br><button id="botao-apagar-filme" onclick="removerFilme(event)">Remover Filme</button>`)
+
+    checkSameMovie = evento.target.id
 }
 
-let contador = 0
-
-function popupFilme(evento) {
-    divPopupFilme.innerHTML = (`<button onclick="fecharPopupFilme()" class="fechar-popup">x</button>`)
-    switch (evento.target.id) {
-        case 'filme-1':
-            if (informacoesFilmes[0]) {
-                for (i = 1; i < 6; i++) {
-                    divPopupFilme.innerHTML += (`${informacoesFilmes[i]}<br>`)
-                }
-                divPopupFilme.classList.remove('oculta')
-            }
-            contador = 1
-            divPopupFilme.innerHTML += (`<button onclick="apagarFilme()" id="botao-apagar-filme">Apagar Filme</button>`)
-            break
-        case 'filme-2':
-            if (informacoesFilmes[6]) {
-                for (i = 7; i < 12; i++) {
-                    divPopupFilme.innerHTML += (`${informacoesFilmes[i]}<br>`)
-                    divPopupFilme.classList.remove('oculta')
-                }
-            }
-            contador = 2
-            divPopupFilme.innerHTML += (`<button onclick="apagarFilme()" id="botao-apagar-filme">Apagar Filme</button>`)
-            break
-        case 'filme-3':
-            if (informacoesFilmes[12]) {
-                for (i = 13; i < 18; i++) {
-                    divPopupFilme.innerHTML += (`${informacoesFilmes[i]}<br>`)
-                    divPopupFilme.classList.remove('oculta')
-                }
-            }
-            contador = 3
-            divPopupFilme.innerHTML += (`<button onclick="apagarFilme()" id="botao-apagar-filme">Apagar Filme</button>`)
-            break
-        case 'filme-4':
-            if (informacoesFilmes[18]) {
-                for (i = 19; i < 24; i++) {
-                    divPopupFilme.innerHTML += (`${informacoesFilmes[i]}<br>`)
-                    divPopupFilme.classList.remove('oculta')
-                }
-            }
-            contador = 4
-            divPopupFilme.innerHTML += (`<button onclick="apagarFilme()" id="botao-apagar-filme">Apagar Filme</button>`)
-            break
-        case 'filme-5':
-            if (informacoesFilmes[24]) {
-                for (i = 25; i < 30; i++) {
-                    divPopupFilme.innerHTML += (`${informacoesFilmes[i]}<br>`)
-                    divPopupFilme.classList.remove('oculta')
-                }
-            }
-            contador = 5
-            divPopupFilme.innerHTML += (`<button onclick="apagarFilme()" id="botao-apagar-filme">Apagar Filme</button>`)
-            break
-        case 'filme-6':
-            if (informacoesFilmes[30]) {
-                for (i = 31; i < 36; i++) {
-                    divPopupFilme.innerHTML += (`${informacoesFilmes[i]}<br>`)
-                    divPopupFilme.classList.remove('oculta')
-                }
-            }
-            contador = 6
-            divPopupFilme.innerHTML += (`<button onclick="apagarFilme()" id="botao-apagar-filme">Apagar Filme</button>`)
-            break
-        case 'filme-7':
-            if (informacoesFilmes[36]) {
-                for (i = 37; i < 42; i++) {
-                    divPopupFilme.innerHTML += (`${informacoesFilmes[i]}<br>`)
-                    divPopupFilme.classList.remove('oculta')
-                }
-            }
-            contador = 7
-            divPopupFilme.innerHTML += (`<button onclick="apagarFilme()" id="botao-apagar-filme">Apagar Filme</button>`)
-            break
-        case 'filme-8':
-            if (informacoesFilmes[42]) {
-                for (i = 43; i < 48; i++) {
-                    divPopupFilme.innerHTML += (`${informacoesFilmes[i]}<br>`)
-                    divPopupFilme.classList.remove('oculta')
-                }
-            }
-            contador = 8
-            divPopupFilme.innerHTML += (`<button onclick="apagarFilme()" id="botao-apagar-filme">Apagar Filme</button>`)
-            break
-        case 'filme-9':
-            if (informacoesFilmes[48]) {
-                for (i = 49; i < 54; i++) {
-                    divPopupFilme.innerHTML += (`${informacoesFilmes[i]}<br>`)
-                    divPopupFilme.classList.remove('oculta')
-                }
-            }
-            contador = 9
-            divPopupFilme.innerHTML += (`<button onclick="apagarFilme()" id="botao-apagar-filme">Apagar Filme</button>`)
-            break
-        case 'filme-10':
-            if (informacoesFilmes[54]) {
-                for (i = 55; i < 60; i++) {
-                    divPopupFilme.innerHTML += (`${informacoesFilmes[i]}<br>`)
-                    divPopupFilme.classList.remove('oculta')
-                }
-            }
-            contador = 10
-            divPopupFilme.innerHTML += (`<button onclick="apagarFilme()" id="botao-apagar-filme">Apagar Filme</button>`)
-            break
-        case 'filme-11':
-            if (informacoesFilmes[60]) {
-                for (i = 61; i < 66; i++) {
-                    divPopupFilme.innerHTML += (`${informacoesFilmes[i]}<br>`)
-                    divPopupFilme.classList.remove('oculta')
-                }
-            }
-            contador = 11
-            divPopupFilme.innerHTML += (`<button onclick="apagarFilme()" id="botao-apagar-filme">Apagar Filme</button>`)
-            break
-        default:
-            break
-    }
-}
-
-function apagarFilme() {
-    switch (contador) {
-        case 1:
-            informacoesFilmes.splice(0, 6)
-            break
-        case 2:
-            informacoesFilmes.splice(6, 6)
-            break
-        case 3:
-            informacoesFilmes.splice(12, 6)
-            break
-        case 4:
-            informacoesFilmes.splice(18, 6)
-            break
-        case 5:
-            informacoesFilmes.splice(24, 6)
-            break
-        case 6:
-            informacoesFilmes.splice(30, 6)
-            break
-        case 7:
-            informacoesFilmes.splice(36, 6)
-            break
-        case 8:
-            informacoesFilmes.splice(42, 6)
-            break
-        case 9:
-            informacoesFilmes.splice(48, 6)
-            break
-        case 10:
-            informacoesFilmes.splice(54, 6)
-            break
-        case 11:
-            informacoesFilmes.splice(60, 6)
-            break
-    }
-    divPopupFilme.classList.add('oculta')
-    arrayConvertido = JSON.stringify(informacoesFilmes)
-    localStorage.setItem('arraySalvo', arrayConvertido)
-    switchCarregar()
-}
-
-function clearLocalStorage() {
-    localStorage.clear()
+function removerFilme(evento) {
+    arrayFilmes.splice(evento.target.id,1)
+    fecharPopupFilme()
+    addDivFilmes()
 }
